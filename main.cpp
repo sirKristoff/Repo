20ea644972d488d608f60bd5f506456f65348e6a
Rysiu <krzysztof.j.lasota@gmail.com>
2013-03-23 23:16:11 +0100

    Data i notka

--- main.cpp
+++ main.cpp
@@ -1,9 +1,12 @@
 /**
  * @file main.cpp
+ * @date 23.03.2013
  * @author Krzysztof Lasota
  * @brief Prosty program do testowania
  * 
  * Opis programu.
+ *
+ * @note To tylko progam treningowy
  */
 
 
