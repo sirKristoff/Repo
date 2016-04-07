31024f6bae9ca9b04f214d98042e4116cfa31f3c
Krzysztof Lasota <krzysztof.j.lasota@gmail.com>
2016-04-07 11:55:08 +0200

    Main file

---
+++ main.cpp
@@ -0,0 +1,24 @@
+/**
+ * @file main.cpp
+ * @date 23.03.2013
+ * @author Krzysztof Lasota
+ * @brief Prosty program do testowania
+ * 
+ * Opis programu.
+ *
+ * @note To tylko progam treningowy
+ */
+
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char* argv[])
+{
+  return 0;
+}
+
+void hello()
+{
+	return;
+}
